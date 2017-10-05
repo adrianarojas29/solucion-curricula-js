492fa5fd90769309565aba1f88826c648233ad87
adrianarojas29 <adriana_29-98@hotmail.com>
2017-10-05 16:39:02 -0500

    Solución de array-to-string.js.

--- array-to-string.js
+++ array-to-string.js
@@ -2,5 +2,5 @@
 Crear una funcion que reciba como parametro un array y retorne una cadena
 -> retorne quack! sneeze! boom!
 */
-var arr = ["quack","sneeze","boom"];
- console.log(arr.join());
+var arr = ["quack","sneeze","boom"," "];
+ console.log(arr.join('! '));
